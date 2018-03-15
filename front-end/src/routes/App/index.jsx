ca2980c50fba6c56959e91e8878a5619a27b67ce
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-03-15 14:18:55 +0200

    Fixed couple of build errors

--- front-end/src/routes/App/index.jsx
+++ front-end/src/routes/App/index.jsx
@@ -1,37 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Route, Switch, NavLink } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import Text from '../../components/Text'
 
-class App extends Component {
-  componentDidMount () {
-    this.props.setFallback('/')
-  }
+const renderMain = () => (
+  <div className='searchContainer'>
+    <Text text="Welcome" />
+  </div>
+)
 
-  search () {
-    return (
-      <div className='searchContainer'>
-        <Text text="Welcome" />
-      </div>
-    )
-  }
-
-  render () {
-
-    return (
-      <div className='container'>
-
-        <Route exact path='/' render={() => this.search()} />
-
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className='container'>
+    <Route exact path='/' render={renderMain} />
+  </div>
+)
 
 App.propTypes = {
-  error: PropTypes.object,
-  setFallback: PropTypes.func.isRequired
 }
 
 export default App
